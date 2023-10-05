40c0394579df8ff5bc454eaa795967e9dc3ef7d1
AdalidIrvin2022 <adalidirvin@outlook.com>
2023-10-05 16:32:16 -0400

    comentaarios+intentos

--- js/codigo.js
+++ js/codigo.js
@@ -1,26 +1,31 @@
-a=parseInt(Math.random()*50)
-b=parseInt(Math.random()*50)
+let a=parseInt(Math.random()*50)
+let b=parseInt(Math.random()*50)
 document.getElementById("n1").value=a
 document.getElementById("n2").value=b
 
+let intentos=3
+
 function generar(){
-    dividendo=parseInt(Math.random()*50)
-    divisor=dividendo*(parseInt(Math.random()*8)+2)
+    //Para asegurarnos que´el resultado de la división sea un número entero, generamos los dos valores a partir del multiplo de otro.  
+    dividendo=parseInt(Math.random()*50)//Generamos un valor alatorio entre 0 y 50
+    divisor=dividendo*(parseInt(Math.random()*8)+2) //Generamos un valor aleatorio entre 2 y 8
     document.getElementById("d2").value=dividendo
     document.getElementById("d1").value=divisor
 }
-
-
-
 
 
 function verificar(){
     resultado=document.getElementById("resultado").value
     console.log(a)
     console.log(b)
-    if(resultado==(a+b)){
-        document.getElementById("mensaje").value="CORRECT0"
+    if(intentos>0){
+        if(resultado==(a+b)){
+            document.getElementById("mensaje").value="CORRECT0"
+        }else{
+            document.getElementById("mensaje").value="INCORRECTO"
+            intentos=intentos-1
+        }
     }else{
-        document.getElementById("mensaje").value="INCORRECTO"
+        document.getElementById("mensaje").value="YA NO QUEDAN INTENTOS"
     }
 }
