ffca6829c48921f0ed9d61e035ca35ad2005f7eb
AdalidIrvin2022 <adalidirvin@outlook.com>
2023-10-05 16:22:35 -0400

    DIVISION

--- js/codigo.js
+++ js/codigo.js
@@ -3,6 +3,16 @@ b=parseInt(Math.random()*50)
 document.getElementById("n1").value=a
 document.getElementById("n2").value=b
 
+function generar(){
+    dividendo=parseInt(Math.random()*50)
+    divisor=dividendo*(parseInt(Math.random()*8)+2)
+    document.getElementById("d2").value=dividendo
+    document.getElementById("d1").value=divisor
+}
+
+
+
+
 
 function verificar(){
     resultado=document.getElementById("resultado").value
